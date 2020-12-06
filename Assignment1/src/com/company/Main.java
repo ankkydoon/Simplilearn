3c6036fc5cb6e7fb107bf3482c55199573b14912
ankkydoon <bhattankit2008@gmail.com>
2020-12-06 20:42:59 +0530

    Add files via upload

---
+++ Assignment1/src/com/company/Main.java
@@ -0,0 +1,78 @@
+package com.company;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class Main {
+    public static String workingDir;
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Welcome to Windows File Viewer!");
+
+         workingDir= System.getProperty("user.dir");
+
+        int option = 0;
+
+        while (option != 4) {
+            Scanner myObj = new Scanner(System.in);
+            System.out.println("\n");
+            System.out.println("Press 1 for viewing documents");
+            System.out.println("Press 2 for creating document");
+            System.out.println("Press 3 for deleting document");
+            System.out.println("Press 4 for exit");
+
+            try {
+                option = Integer.parseInt(myObj.nextLine());
+            } catch (NumberFormatException ex) {
+                System.out.println(ex.getMessage());
+            }
+            switch (option) {
+                case 1:
+                    viewDocument();
+                    break;
+                case 2:
+                    System.out.println("Enter name of file.\n");
+                    createDocument(myObj.nextLine());
+                    break;
+                case 3:
+                    System.out.println("Enter name of file.\n");
+                    deleteDocument(myObj.nextLine());
+                case 4:
+                    break;
+                default:
+                    System.out.println("Enter correct option");
+            }
+        }
+    }
+
+    private static void deleteDocument(String fileName) {
+        File targetFile = new File(fileName);
+        targetFile.delete();
+    }
+
+    private static void createDocument(String fileName) throws IOException {
+        File newFile = new File(fileName);
+        boolean success = newFile.createNewFile();
+        assert(success);
+    }
+
+    private static void viewDocument() {
+        System.out.println("\n");
+        System.out.println("Viewing Files in Current Working Directory = " + workingDir);
+        try (Stream<Path> paths = Files.walk(Paths.get(workingDir))) {
+            paths
+                    .filter(Files::isRegularFile)
+                    .forEach(System.out::println);
+            System.out.println("\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
